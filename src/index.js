b6b7c8d826d99cc8b6ea5cbda35341e1766e423f
Jlocbka <e.lubimov92@gmail.com>
2017-05-23 20:58:06 +0300

    add all quest else '*'

--- src/index.js
+++ src/index.js
@@ -1,82 +1,84 @@
-/* ДЗ 1 - Функции */
+/* ДЗ 5 - DOM Events */
 
-/*
- Задание 1:
-
- Функция должна принимать один аргумент и возвращать его
+/**
+ * Функция должна добавлять обработчик fn события eventName к элементу target
+ *
+ * @param {string} eventName - имя события, на которое нужно добавить обработчик
+ * @param {Element} target - элемент, на который нужно добавить обработчик
+ * @param {function} fn - обработчик
  */
-function returnFirstArgument(arg) {
-    return arg;
+function addListener(eventName, target, fn) {
+    target.addEventListener(eventName, fn);
 }
 
-/*
- Задание 2:
-
- Функция должна принимать два аргумента и возвращать сумму переданных значений
- Значение по умолчанию второго аргумента должно быть 100
+/**
+ * Функция должна удалять обработчик fn события eventName у элемента target
+ *
+ * @param {string} eventName - имя события, для которого нужно удалить обработчик
+ * @param {Element} target - элемент, у которого нужно удалить обработчик
+ * @param {function} fn - обработчик
  */
-function defaultParameterValue(a, b = 100) {
-    var c = a + b;
-    return c;
+function removeListener(eventName, target, fn) {
+    target.removeEventListener(eventName, fn);
 }
 
-/*
- Задание 3:
-
- Функция должна возвращать все переданные в нее аргументы в виде массива
- Количество переданных аргументов заранее неизвестно
+/**
+ * Функция должна добавлять к target обработчик события eventName, который должен отменять действие по умолчанию
+ *
+ * @param {string} eventName - имя события, для которого нужно удалить обработчик
+ * @param {Element} target - элемент, на который нужно добавить обработчик
  */
-function returnArgumentsArray(arr = []) {
-    var arr = [];
-    for (var i = 0; i < arguments.length; i++) {
-        arr.push(arguments[i]);
+function skipDefault(eventName, target) {
+    target.addEventListener(eventName, function (e) {
+        e.preventDefault();
+    });
+}
+
+/**
+ * Функция должна эмулировать событие click для элемента target
+ *
+ * @param {Element} target - элемент, на который нужно добавить обработчик
+ */
+function emulateClick(target) {
+    var event = new Event('click');
+        target.dispatchEvent(event);
+
+}
+
+/**
+ * Функция должна добавить такой обработчик кликов к элементу target
+ * который реагирует (вызывает fn) только на клики по элементам BUTTON внутри target
+ *
+ * @param {Element} target - элемент, на который нужно добавить обработчик
+ * @param {function} fn - функция, которую нужно вызвать при клике на элемент BUTTON внутри target
+ */
+function delegate(target, fn) {
+    debugger;
+    for(var i = 0; target.children.length > i; i++) {
+        if(target.children[i].localName == 'button') {
+            var event = target.children[i];
+        }
     }
-    return arr;
+    event.addEventListener('click', fn);
 }
 
-/*
- Задание 4:
-
- Функция должна принимать другую функцию и возвращать результат вызова переданной функции
+/**
+ * *** Со звездочкой ***
+ * Функция должна добавить такой обработчик кликов к элементу target
+ * который сработает только один раз и удалится
+ * Постарайтесь не создавать глобальных переменных
+ *
+ * @param {Element} target - элемент, на который нужно добавить обработчик
+ * @param {function} fn - обработчик
  */
-function returnFnResult(fn) {
-    return fn();
+function once(target, fn) {
 }
 
-/*
- Задание 5:
-
- Функция должна принимать число (значение по умолчанию - 0) и возвращать функцию (F)
- При вызове F, переданное число должно быть увеличено на единицу и возвращено из F
- */
-function returnCounter(number = 0) {
-    var F = function () {
-        number++;
-        return number;
-    };
-   return F;
-}
-
-/*
- Задание 6 *:
-
- Функция должна принимать другую функцию (F) и некоторое количество дополнительных аргументов
- Функция должна привязать переданные аргументы к функции F и вернуть получившуюся функцию
- */
-function bindFunction(fn) {
-
-}
-
-function F () {
-}
-
-bindFunction(F());
-var f = F.bind(null, 1, 2, 3,);
 export {
-    returnFirstArgument,
-    defaultParameterValue,
-    returnArgumentsArray,
-    returnFnResult,
-    returnCounter,
-    bindFunction
-}
+    addListener,
+    removeListener,
+    skipDefault,
+    emulateClick,
+    delegate,
+    once
+};
